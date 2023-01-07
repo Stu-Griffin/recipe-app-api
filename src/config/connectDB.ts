38123667667448ea72bec716599c60635676a931
Stu-Griffin <maxprogrammer39@gmail.com>
2023-01-07 16:47:01 +0200

    Deleted trash added error image for receip and added DB link as a string

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -5,7 +5,7 @@ const connectDB = async () => {
 	try {
 		mongoose.set(`strictQuery`, true);
 		if(process.env.URL) {
-			await mongoose.connect(process.env.URL);
+			await mongoose.connect(`mongodb+srv://adminus:1234567890@cluster0.1uyhaqy.mongodb.net/?retryWrites=true&w=majority`);
 			console.log(`Connected to DB`);
 		} else {
 			console.log(`You forgot add dotenv's variables`);
